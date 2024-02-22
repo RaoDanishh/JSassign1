36a11d6f3a315bea2bb6ade6d05dc5e2625baed1
RaoDanishh <147903668+RaoDanishh@users.noreply.github.com>
2024-02-22 17:35:34 +0500

    commit

---
+++ app.js
@@ -0,0 +1,65 @@
+
+alert("Hello, welcome to the first assignment of JS ")
+
+// assignment question no-1
+
+let username="";
+
+// 2nd Q
+
+let myName= "H D Rehman"
+
+// 3rd q
+
+document.getElementById("1stbutton").onclick= function () {
+    let hello= "HELLO WORLD"
+    alert(hello);
+
+}
+
+//  4th Q
+
+document.getElementById("2ndbutton").onclick= function () {
+    let bioData = "danish, 203460, WMA2023"
+    alert(bioData);
+}
+
+// 5th
+
+document.getElementById("email").onclick= function () {
+    let Email = "danishrao299@gmail.com"
+    alert(Email);
+}
+
+// 6th
+
+document.getElementById("book").onclick= function () {
+    let Book = " A masrter way to learn JavaScript "
+    alert(Book);
+}
+
+// 7th
+
+document.write("There is some text that written by direct from js file");
+
+// 8th
+
+
+document.getElementById("logo").onclick= function () {
+    let logo = " ▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+    alert(logo);
+}
+
+
+
+// 9th
+
+document.getElementById("number").onclick= function () {
+    let Number ='0303-2872912'
+    alert(Number);
+}
+
+
+
+// 10
+
